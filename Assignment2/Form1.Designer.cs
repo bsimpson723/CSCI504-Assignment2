f51dd24f37dc269fc24b2fd076314cae54883413
Ben Simpson <Bsimpson@imo-online.com>
2018-09-20 09:58:59 -0500

    started building out form... no functionality

--- Assignment2/Form1.Designer.cs
+++ Assignment2/Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Title = new System.Windows.Forms.Label();
+            this.studnetList = new System.Windows.Forms.ListBox();
+            this.courseList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(308, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(406, 26);
+            this.Title.TabIndex = 0;
+            this.Title.Text = "NIU Enrollment Management System";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // studnetList
+            // 
+            this.studnetList.FormattingEnabled = true;
+            this.studnetList.Location = new System.Drawing.Point(575, 59);
+            this.studnetList.Name = "studnetList";
+            this.studnetList.Size = new System.Drawing.Size(192, 433);
+            this.studnetList.TabIndex = 1;
+            // 
+            // courseList
+            // 
+            this.courseList.FormattingEnabled = true;
+            this.courseList.Location = new System.Drawing.Point(782, 59);
+            this.courseList.Name = "courseList";
+            this.courseList.Size = new System.Drawing.Size(192, 433);
+            this.courseList.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1013, 656);
+            this.Controls.Add(this.courseList);
+            this.Controls.Add(this.studnetList);
+            this.Controls.Add(this.Title);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Title;
+        private System.Windows.Forms.ListBox studnetList;
+        private System.Windows.Forms.ListBox courseList;
     }
 }
 
